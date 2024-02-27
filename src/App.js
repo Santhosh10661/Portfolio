eb621e77f757af900248c04114a37feefde90bb3
santhosh <santhosh10661@gmail.com>
2024-02-27 09:54:07 +0530

    navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Home from "./Home";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate("/", { replace: true });
+  }, []);
+
   return (
     <div className="container-fluid App p-0">
       <Home />
